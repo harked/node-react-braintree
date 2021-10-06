59ba91860798d5411ba8a333b2872e2461cfd792
Randy Harkediansa <randy.harkediansa@gmail.com>
2021-10-06 08:34:47 +0800

    Add src index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
